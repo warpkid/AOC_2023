a25ff18def6f771862fbfeac924861d725f5c421
Sergio Russo <sergio.russo@gmail.com>
2023-12-03 13:06:52 +0000

    Refactor game result calculation and set processing

--- day2/code.fsx
+++ day2/code.fsx
@@ -2,7 +2,7 @@ open System.IO
 
 let lines = File.ReadAllLines("./day2/input.txt")
 
-let processString (game: int) (str: string) =
+let processString (game: int) (str: string) : int * string array =
     game, str.Substring(str.IndexOf(":") + 2).Split(";")
 
 type Ball =
@@ -12,59 +12,38 @@ type Ball =
 
 let bag = [ Red, 12; Green, 13; Blue, 14 ] |> Map.ofList
 
-let getGameResult (count: int) (colour: string) =
-    printfn "Colour: %s Count: %i" colour count
-
+let getGameResult (count: int) (colour: string) : int * Ball =
     match colour.ToLower() with
-    | "red" -> Red, count
-    | "blue" -> Blue, count
-    | "green" -> Green, count
+    | "red" -> count, Red
+    | "blue" -> count, Blue
+    | "green" -> count, Green
     | _ -> failwith "Unknown Colour"
 
-let getResultForSet (set: string) =
+let getResultForSet (set: string) : (int * Ball) array =
     set.Split(",")
-    |> Array.map (fun r -> r.Trim().Split(" "))
-    |> Array.map (fun a -> a[0], a[1])
-    |> Array.map (fun (count, colour) -> getGameResult (int count) colour)
+    |> Array.map (fun r ->
+        let a = r.Trim().Split(" ")
+        let count, colour = int a.[0], a.[1]
+        getGameResult count colour)
 
-let maxSet (results: (Ball * int) array) =
+let maxSet (results: (int * Ball) array) =
     let state = [ Red, 0; Green, 0; Blue, 0 ] |> Map.ofList
-    printfn "Summing Set"
 
     results
     |> Array.fold
-        (fun acc r ->
-            match r with
-            | Red, count ->
-                if count > (Map.find Red acc) then
-                    Map.add Red count acc
-                else
-                    acc
-            | Blue, count ->
-                if count > (Map.find Blue acc) then
-                    Map.add Blue count acc
-                else
-                    acc
-            | Green, count ->
-                if count > (Map.find Green acc) then
-                    Map.add Green count acc
-                else
-                    acc)
+        (fun acc (count, ball) ->
+            let currentCount = Map.find ball acc
+            if count > currentCount then Map.add ball count acc else acc)
         state
 
 let results =
     lines
     |> Array.mapi (fun i l -> processString (i + 1) l)
     |> Array.map (fun (game, sets) ->
-        printfn "Game Info %i %A" game sets
-        let result = sets |> Array.map getResultForSet
+        let result = sets |> Array.map getResultForSet |> Array.collect id |> maxSet
         game, result)
-    |> Array.map (fun (game, result) ->
-        let squashed = result |> Array.collect id |> maxSet
-        game, squashed)
 
 let isPossible gameNumber bag result =
-    printfn "Game: %i %A %A" gameNumber bag result
     gameNumber, Map.forall (fun key value -> value >= (Map.find key bag)) result
 
 
@@ -72,17 +51,10 @@ let isPossible gameNumber bag result =
 
 results
 |> Array.map (fun (gameNumber, results) -> isPossible gameNumber results bag)
-|> Array.map (fun (g, r) ->
-    printfn "Game: %i Valid: %b" g r
-    g, r)
-|> Array.sumBy (fun (g, v) -> if v = true then g else 0)
+|> Array.sumBy (fun (game, isValid) -> if isValid then game else 0)
 
 // powers
 
 results
 |> Array.map (fun (_, map) -> map |> Map.values |> Seq.reduce (fun v a -> v * a))
 |> Array.sum
-
-
-// guesses
-// 2348
